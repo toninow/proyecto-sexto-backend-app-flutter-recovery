5d8714cdbb37a600ebc32199384e07ff7296ab61
FunnyIonic <negdi0mn@gmail.com>
2020-07-14 16:53:54 +0800

    Procfile for Heroku

--- database/migrations/2020_07_14_083337_make_shippings_table.php
+++ database/migrations/2020_07_14_083337_make_shippings_table.php
@@ -14,6 +14,7 @@ class MakeShippingsTable extends Migration
     public function up()
     {
         //
+		Schema::dropIfExists('shippings');
 		
 		Schema::create('shippings', function (Blueprint $table) {
             $table->bigIncrements('id');
