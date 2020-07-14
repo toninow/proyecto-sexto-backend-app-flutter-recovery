0b66e9b8e12cb554aad486f921ea98366722dfc5
FunnyIonic <negdi0mn@gmail.com>
2020-07-14 13:31:21 +0800

    Make it better

--- routes/api.php
+++ routes/api.php
@@ -25,3 +25,4 @@ Route::get('sliders' , 'Api\SliderController@index');
 Route::get('get-all-hot-products' , 'Api\ProductController@getAllHotProducts');
 Route::get('get-all-new-arrival-products' , 'Api\ProductController@getAllNewArrivalProducts');
 Route::get('get-products-by-category/{categoryId}' , 'Api\ProductController@getProductsByCategoryId');
+Route::post('register' , 'Api\UserController@register');
