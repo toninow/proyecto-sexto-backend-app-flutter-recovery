8b6e7c68b38c3687db10f0abb57e822b88b657ea
FunnyIonic <negdi0mn@gmail.com>
2020-07-15 14:41:04 +0800

    Procfile for Heroku

--- app/Http/Controllers/Api/PaymentController.php
+++ app/Http/Controllers/Api/PaymentController.php
@@ -92,35 +92,44 @@ class PaymentController extends Controller
             $data = $request->input('cartItems');
             $cartItems = json_decode($data, true);
             $totalAmount = 0.0;
-            foreach ($cartItems as $cartItem){
-                $order = new Order();
-                $order->order_date = Carbon::now()->toDateString();
-                $order->product_id = $cartItem['productId'];
-                $order->user_id = $request->input('userId');
-                $order->quantity = $cartItem['productQuantity'];
-                $order->amount = ($cartItem['productPrice'] - $cartItem['productDiscount']);
-                $totalAmount+= $order->amount * $order->quantity;
-                $order->save();
-            }
-            \Stripe\Stripe::setApiKey('sk_test_mirrQ5hTnI8Ggpr6nsHiAY93');
+			
+			if(is_array($cartItems)){
+				foreach ($cartItems as $cartItem){
+					$order = new Order();
+					$order->order_date = Carbon::now()->toDateString();
+					$order->product_id = $cartItem['productId'];
+					$order->user_id = $request->input('userId');
+					$order->quantity = $cartItem['productQuantity'];
+					$order->amount = ($cartItem['productPrice'] - $cartItem['productDiscount']);
+					$totalAmount+= $order->amount * $order->quantity;
+					$order->save();
+				}
+				
+				
+				\Stripe\Stripe::setApiKey('sk_test_mirrQ5hTnI8Ggpr6nsHiAY93');
 
-            $token = \Stripe\Token::create([
-                'card' => [
-                    'number' => $request->input('cardNumber'),
-                    'exp_month' => $request->input('expiryMonth'),
-                    'exp_year' => $request->input('expiryYear'),
-                    'cvc' => $request->input('cvcNumber')
-                ]
-            ]);
+				$token = \Stripe\Token::create([
+					'card' => [
+						'number' => $request->input('cardNumber'),
+						'exp_month' => $request->input('expiryMonth'),
+						'exp_year' => $request->input('expiryYear'),
+						'cvc' => $request->input('cvcNumber')
+					]
+				]);
 
-            $charge = \Stripe\Charge::create([
-                'amount' => $totalAmount * 100,
-                'currency' => 'usd',
-                'source' => $token,
-                'receipt_email' => $request->input('email'),
-            ]);
+				$charge = \Stripe\Charge::create([
+					'amount' => $totalAmount * 100,
+					'currency' => 'usd',
+					'source' => $token,
+					'receipt_email' => $request->input('email'),
+				]);
 
-            return response(['result' => true]);
+				return response(['result' => true]);
+			}
+			
+			return response(['result' => false]);
+            
+            
         } catch (Exception $exception){
             return response(['result' => $exception]);
         }
