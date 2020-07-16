fb586de8ebd20c805c4489744fd99a3fce5b2c49
FunnyIonic <negdi0mn@gmail.com>
2020-07-16 20:47:00 +0800

    new laravel project

---
+++ database/migrations/2020_07_16_124415_drop_orders_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class DropOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+		Schema::dropIfExists('orders');
+        //
+		Schema::create('orders', function (Blueprint $table) {
+            $table->bigIncrements('id');
+			$table->string('order_date');
+			$table->integer('product_id');
+			$table->integer('user_id');
+			$table->integer('quantity');
+			$table->double('amount');
+            $table->timestamps();
+        });
+		
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
