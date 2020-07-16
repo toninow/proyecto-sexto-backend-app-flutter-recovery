9a45c203f125b449715dbfa33bed53bf8cbc6d6d
FunnyIonic <negdi0mn@gmail.com>
2020-07-16 22:16:55 +0800

    Procfile for Heroku

--- routes/web.php
+++ routes/web.php
@@ -17,26 +17,30 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('dashboard', 'DashboardController@index');
-Route::post('post-category-form', 'CategoryController@store');
-Route::get('create-category', 'CategoryController@create');
-Route::get('all-categories', 'CategoryController@index');
-Route::get('edit-category/{id}', 'CategoryController@edit');
-Route::post('update-category/{id}', 'CategoryController@update');
-Route::get('delete-category/{id}', 'CategoryController@destroy');
-Route::get('get-product-form', 'ProductController@create');
-Route::post('post-product-form', 'ProductController@store');
-Route::get('all-products', 'ProductController@index');
-Route::get('edit-product/{id}', 'ProductController@edit');
-Route::post('post-product-edit-form/{id}', 'ProductController@update');
-Route::get('delete-product/{id}', 'ProductController@destroy');
-Route::post('post-slider-form', 'SliderController@store');
-Route::get('get-slider-form', 'SliderController@create');
-Route::get('all-sliders', 'SliderController@index');
-Route::post('post-slider-edit-form/{id}', 'SliderController@update');
-Route::get('edit-slider/{id}', 'SliderController@edit');
-Route::get('delete-slider/{id}', 'SliderController@destroy');
-Route::get('all-orders', 'OrderController@index');
+
+
+Route::middleware(['auth'])->group(function () {
+	Route::get('dashboard', 'DashboardController@index');
+	Route::post('post-category-form', 'CategoryController@store');
+	Route::get('create-category', 'CategoryController@create');
+	Route::get('all-categories', 'CategoryController@index');
+	Route::get('edit-category/{id}', 'CategoryController@edit');
+	Route::post('update-category/{id}', 'CategoryController@update');
+	Route::get('delete-category/{id}', 'CategoryController@destroy');
+	Route::get('get-product-form', 'ProductController@create');
+	Route::post('post-product-form', 'ProductController@store');
+	Route::get('all-products', 'ProductController@index');
+	Route::get('edit-product/{id}', 'ProductController@edit');
+	Route::post('post-product-edit-form/{id}', 'ProductController@update');
+	Route::get('delete-product/{id}', 'ProductController@destroy');
+	Route::post('post-slider-form', 'SliderController@store');
+	Route::get('get-slider-form', 'SliderController@create');
+	Route::get('all-sliders', 'SliderController@index');
+	Route::post('post-slider-edit-form/{id}', 'SliderController@update');
+	Route::get('edit-slider/{id}', 'SliderController@edit');
+	Route::get('delete-slider/{id}', 'SliderController@destroy');
+	Route::get('all-orders', 'OrderController@index');	
+});
 
 Auth::routes();
 
