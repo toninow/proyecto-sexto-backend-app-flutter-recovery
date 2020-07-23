1f2b65b41f4d3fad8bbd264702d014f4290f7721
FunnyIonic <negdi0mn@gmail.com>
2020-07-23 20:19:52 +0800

    new laravel project

--- app/Http/Controllers/Api/PaymentController.php
+++ app/Http/Controllers/Api/PaymentController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\Order;
+use App\Models\OrderDetail;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 
@@ -98,17 +99,37 @@ class PaymentController extends Controller
 			
 			
 				foreach ($cartItems as $cartItem){
-					$order = new Order();
-					$order->order_date = Carbon::now()->toDateString();
-					$order->product_id = $cartItem['productId'];
-					$order->user_id = $request->input('userId');
-					$order->payment_type = $selectedPaymentOption['paymentType'];
-					$order->quantity = $cartItem['productQuantity'];
-					$order->amount = ($cartItem['productPrice'] - $cartItem['productDiscount']);
-					$totalAmount+= $order->amount * $order->quantity;
-					$order->save();
+					
+					//$order->quantity = $cartItem['productQuantity'];
+					//$order->amount = ($cartItem['productPrice'] - $cartItem['productDiscount']);
+					//$totalAmount+= $order->amount * $order->quantity;
+					$totalAmount += ($cartItem['productPrice'] - $cartItem['productDiscount']) * $cartItem['productQuantity'];
+					
 				}
 				
+				
+					$order = new Order();
+					$order->order_date = Carbon::now()->toDateString();
+					$order->user_id = $request->input('userId');
+					$order->payment_type = $selectedPaymentOption['paymentType'];
+					$order->total_amount = $totalAmount;
+					$order->order_id = Carbon::now()->year . Carbon::now()->month . Carbon::now()->day . $request->input('userId');
+					
+					if($order->save()){
+						
+						foreach($cartItems as $cartItem){
+							
+							
+							$orderDetail = new OrderDetail();
+							$orderDetail->order_id = $order->id;
+							$orderDetail->order_product_name = $cartItem['productName'];
+							$orderDetail->product_order_price = $cartItem['productPrice'];
+							$orderDetail->product_photo = $cartItem['productPhoto'];
+							$orderDetail->product_order_discount = $cartItem['productDiscount'];
+							$orderDetail->save();
+						}
+					}
+					//$order->save();
 				
 				if($selectedPaymentOption['paymentType'] == 'Card'){
 					
