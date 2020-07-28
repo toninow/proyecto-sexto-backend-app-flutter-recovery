2c98fc19b666c3e88b2c8bf2df7e6e886187f4aa
FunnyIonic <negdi0mn@gmail.com>
2020-07-28 16:39:14 +0800

    Procfile for Heroku

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -52,7 +52,17 @@ class ProductController extends Controller
         $product->is_hot_product = $request->input('isHotProduct') ? true : false;
         $product->is_new_arrival = $request->input('isNewArrival') ? true : false;
         $product->category_id = $request->input('category');
+		
+		$category_id_info = $request->input('category');
+		$restaurant = Category::find($category_id_info);
+		
+		echo $restaurant->id;
+		
+		$product->restaurant_id = $restaurant->id;
+		
         $product->user_id = 0;
+		
+		/**
         if($product->save()){
 			
 			
@@ -61,25 +71,12 @@ class ProductController extends Controller
 							
 			$product->photo = $cloundary_upload['url'];
 			$product->save();
-			/**
-            $photo = $request->file('productPhoto');
-            if($photo != null){
-                $ext = $photo->getClientOriginalExtension();
-                $fileName = rand(10000, 50000) . '.' . $ext;
-                if($ext == 'jpg' || $ext == 'png'){
-                    if($photo->move(public_path(), $fileName)){
-                        $product = Product::find($product->id);
-                        //$product->photo = url('/') . '/' . $fileName;
-						$product->photo = 'https://www.indianhealthyrecipes.com/wp-content/uploads/2015/10/pizza-recipe-1.jpg';
-                        $product->save();
-                    }
-                }
-
-            }
-			**/
+		
             return redirect()->back()->with('success', 'Product information inserted successfully!');
         }
         return redirect()->back()->with('failed', 'Product information could not be inserted!');
+		
+		**/
     }
 
     /**
---
+++ database/migrations/2020_07_28_083746_upgrade_products_table_list.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpgradeProductsTableList extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+		
+		Schema::dropIfExists('products');
+		
+		Schema::create('products', function (Blueprint $table) {
+            $table->bigIncrements('id');
+			$table->string('name');
+			$table->integer('price');
+			$table->string('photo');
+			$table->integer('discount');
+			$table->boolean('is_hot_product');
+			$table->boolean('is_new_arrival');
+			$table->integer('category_id');
+			$table->integer('user_id');
+			$table->integer('restaurant_id');
+			$table->text('detail');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
