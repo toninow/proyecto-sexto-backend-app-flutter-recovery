754751041a320fd0f030d455eb499f353168b8ca
FunnyIonic <negdi0mn@gmail.com>
2020-08-03 19:21:18 +0800

    new laravel project

--- resources/views/restaurant/index.blade.php
+++ resources/views/restaurant/index.blade.php
@@ -83,6 +83,24 @@
           </div>
 
         </div>
+		
+		
+		
+		  <script src="{{ URL::to('vendor/jquery/jquery.min.js') }}"></script>
+		  <script src="{{ URL::to('vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+
+		  <!-- Core plugin JavaScript-->
+		  <script src="{{ URL::to('vendor/jquery-easing/jquery.easing.min.js') }}"></script>
+
+		  <!-- Custom scripts for all pages-->
+		  <script src="{{ URL::to('js/sb-admin-2.min.js') }}"></script>
+
+		  <!-- Page level plugins -->
+		  <script src="{{ URL::to('vendor/datatables/jquery.dataTables.min.js') }}"></script>
+		  <script src="{{ URL::to('vendor/datatables/dataTables.bootstrap4.min.js') }}"></script>
+
+		  <!-- Page level custom scripts -->
+		  <script src="{{ URL::to('js/demo/datatables-demo.js') }}"></script>
 	<!-- /.container-fluid -->
 
 @stop
