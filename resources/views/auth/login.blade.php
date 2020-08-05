3612f1f4f54c85464a921c5b2a827405cc909071
FunnyIonic <negdi0mn@gmail.com>
2020-08-05 21:57:59 +0800

    new laravel project

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -15,11 +15,11 @@
   <title>SB Admin 2 - Login</title>
 
   <!-- Custom fonts for this template-->
-  <link href="{{ URL::to('vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet" type="text/css">
+  <link href="{{ URL::asset('vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet" type="text/css">
   <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
   <!-- Custom styles for this template-->
-  <link href="{{ URL::to('css/sb-admin-2.min.css') }}" rel="stylesheet">
+  <link href="{{ URL::asset('css/sb-admin-2.min.css') }}" rel="stylesheet">
 
 </head>
 
@@ -123,14 +123,14 @@
 
   
   <!-- Bootstrap core JavaScript-->
-  <script src="{{ URL::to('vendor/jquery/jquery.min.js') }}"></script>
-  <script src="{{ URL::to('vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+  <script src="{{ URL::asset('vendor/jquery/jquery.min.js') }}"></script>
+  <script src="{{ URL::asset('vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
 
   <!-- Core plugin JavaScript-->
-  <script src="{{ URL::to('vendor/jquery-easing/jquery.easing.min.js') }}"></script>
+  <script src="{{ URL::asset('vendor/jquery-easing/jquery.easing.min.js') }}"></script>
 
   <!-- Custom scripts for all pages-->
-  <script src="{{ URL::to('js/sb-admin-2.min.js') }}"></script>
+  <script src="{{ URL::asset('js/sb-admin-2.min.js') }}"></script>
  
  </body>
 
