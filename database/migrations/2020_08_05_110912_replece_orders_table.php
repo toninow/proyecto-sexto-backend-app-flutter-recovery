4cacefa99844cbb5c698fa4e7dce7e07888d07c7
FunnyIonic <negdi0mn@gmail.com>
2020-08-05 19:11:46 +0800

    new laravel project

---
+++ database/migrations/2020_08_05_110912_replece_orders_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class RepleceOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+		Schema::dropIfExists('orders');
+        //
+		Schema::create('orders', function (Blueprint $table) {
+            $table->bigIncrements('id');
+			$table->string('order_date');
+			$table->integer('user_id');
+			$table->double('total_amount');
+			$table->string('payment_type');
+			$table->string('order_id');
+			$table->string('status');
+			$table->integer('address_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
