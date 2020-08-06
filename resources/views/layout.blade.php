e75d33749c8568cb04034e03de4e4db0f6f0cc2c
FunnyIonic <negdi0mn@gmail.com>
2020-08-06 12:39:44 +0800

    new laravel project

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -11,6 +11,11 @@
 
   <title>MULTI RESTAURANT - SUPER ADMIN DASHBOARD</title>
 
+  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+  <link href="css/sb-admin-2.min.css" rel="stylesheet">
+  <script src="ckeditor/ckeditor.js"></script>
+  <link href="vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
+  
   <!-- Custom fonts for this template-->
   <link href="{{ URL::to('vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet" type="text/css">
   <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
@@ -197,7 +202,7 @@
             <li class="nav-item dropdown no-arrow">
               <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                 <span class="mr-2 d-none d-lg-inline text-gray-600 small">SUPER ADMIN</span>
-                <img class="img-profile rounded-circle" src="https://source.unsplash.com/QAB-WJcbgJk/60x60">
+                <img class="img-profile rounded-circle" src="https://i.ibb.co/zFn5wmC/download.png">
               </a>
               <!-- Dropdown - User Information -->
               <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
@@ -295,6 +300,23 @@
     </div>
   </div>
 
+  
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+  <script src="js/sb-admin-2.min.js"></script>
+  <script src="vendor/chart.js/Chart.min.js"></script>
+  <script src="js/demo/chart-area-demo.js"></script>
+  <script src="js/demo/chart-pie-demo.js"></script>
+   <!-- Page level plugins -->
+   <script src="vendor/datatables/jquery.dataTables.min.js"></script>
+   <script src="vendor/datatables/dataTables.bootstrap4.min.js"></script>
+
+		  <!-- Page level custom scripts -->
+	<script src="js/demo/datatables-demo.js"></script>
+  
+  
+  
   <!-- Bootstrap core JavaScript-->
   <script src="{{ URL::to('vendor/jquery/jquery.min.js') }}"></script>
   <script src="{{ URL::to('vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
