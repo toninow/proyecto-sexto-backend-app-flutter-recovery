70e7d70f30173000eae3246a2a18285454178688
FunnyIonic <negdi0mn@gmail.com>
2020-08-06 13:21:12 +0800

    new laravel project

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -38,7 +38,11 @@
       <div class="card-body p-0">
         <!-- Nested Row within Card Body -->
         <div class="row">
-          <div class="col-lg-5 d-none d-lg-block bg-register-image"></div>
+          <div class="col-lg-5 d-none d-lg-block">
+		  
+		  
+		  <img src="https://i.ibb.co/VtJP4zT/playstore.png" alt="playstore" border="0">
+		  </div>
           <div class="col-lg-7">
             <div class="p-5">
               <div class="text-center">
